e2081440453547f02e4fd8b3a1e36776fa2145bb
Marc Charette <mcharette@crcemail.com>
2017-09-14 17:06:39 -0400

    Created test_branch. Added WoW patch notes

--- patches_the_bot.py
+++ patches_the_bot.py
@@ -14,6 +14,7 @@ reddit = praw.Reddit('bot1')
 pubg = reddit.subreddit('pubattlegrounds')
 heroes = reddit.subreddit('heroesofthestorm')
 lol = reddit.subreddit('leagueoflegends')
+wow = reddit.subreddit('wow')
 
 def main():
     getFoundPosts() 
@@ -58,6 +59,16 @@ def main():
                 submit("LoL - PBE", "PC", submission)
             else: 
                 print "[INFO] Most recent post for LoL - PBE already found..."
+            break
+    # WoW
+    print "[INFO] Finding patch notes for World of Warcraft..."
+    submissions = wow.submissions(start=submission_interval)
+    for submission in submissions:
+        if 'Patch Notes - WoW'.lower() in submission.title.lower():
+            if submission.id not in foundPosts:
+                submit("World of Warcraft", "PC", submission)
+            else:
+                print "[INFO] Most recent post for World of Warcraft already found..."
             break
 
     print "[INFO] Finished finding patch notes!"
