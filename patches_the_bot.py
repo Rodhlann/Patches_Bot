c244edb8a4ae85f38783022290583b04b3a56151
Tim Pepper <tjpepper93@gmail.com>
2017-09-14 22:46:28 -0400

    Add logging functionality

--- patches_the_bot.py
+++ patches_the_bot.py
@@ -5,6 +5,9 @@ import json
 import sys 
 import os 
 from praw.exceptions import APIException
+import logging 
+
+logging.basicConfig(filename='Patches.log',level=logging.INFO)
 
 # 1 Month 
 submission_interval = time.mktime((dt.date.today() - dt.timedelta(30)).timetuple())
@@ -14,7 +17,7 @@ try:
     reddit = praw.Reddit('bot1')
     reddit.user.me()
 except Exception as e:
-    print ("[UNCAUGHT ERROR] " + e.message)
+    logging.error(e.message)
     sys.exit() 
 
 pubg = reddit.subreddit('pubattlegrounds')
@@ -24,10 +27,10 @@ wow = reddit.subreddit('wow')
 
 def main():
     getFoundPosts() 
-    print ("[INFO] Initiating search")
+    logging.info("Initiating search")
 
     # PUBG
-    print ("[INFO] Finding patch notes for PUBG...")
+    logging.info("Finding patch notes for PUBG...")
     submissions = pubg.submissions(start=submission_interval)
     for submission in submissions:
         if ('Early Access -'.lower() in submission.title.lower() 
@@ -36,10 +39,10 @@ def main():
             if submission.id not in foundPosts: 
                 submit("PlayerUnknown's Battlegrounds", "PC", submission)
             else: 
-                print ("[INFO] Most recent post for PUBG already found...")
+                logging.info("Most recent post for PUBG already found...")
             break    
     # HEROES
-    print ("[INFO] Finding patch notes for HotS...")
+    logging.info("Finding patch notes for HotS...")
     submissions = heroes.submissions(start=submission_interval)
     for submission in submissions:
         if ('us.battle.net'.lower() in submission.url.lower() 
@@ -47,20 +50,20 @@ def main():
             if submission.id not in foundPosts: 
                 submit("Heroes of the Storm", "PC", submission)
             else:
-                print ("[INFO] Most recent post for HotS already found...")
+                logging.info("Most recent post for HotS already found...")
             break
     # LoL
-    print ("[INFO] Finding patch notes for LoL...")
+    logging.info("Finding patch notes for LoL...")
     submissions = lol.submissions(start=submission_interval)
     for submission in submissions:
         if ('leagueoflegends.com/en/news/game-updates/patch/'.lower() in submission.url.lower()):
             if submission.id not in foundPosts: 
                 submit("League of Legends", "PC", submission)
             else:
-                print ("[INFO] Most recent post for LoL already found...")
+                logging.info("Most recent post for LoL already found...")
             break
     # LoL - PBE
-    print ("[INFO] Finding patch notes for LoL - PBE...")
+    logging.info("Finding patch notes for LoL - PBE...")
     submissions = lol.submissions(start=submission_interval)
     for submission in submissions:
         if ('surrenderat20'.lower() in submission.url.lower() 
@@ -68,10 +71,10 @@ def main():
             if submission.id not in foundPosts: 
                 submit("League of Legends - PBE", "PC", submission)
             else: 
-                print ("[INFO] Most recent post for LoL - PBE already found...")
+                logging.info("Most recent post for LoL - PBE already found...")
             break
     # WoW
-    print ("[INFO] Finding patch notes for World of Warcraft...")
+    logging.info("Finding patch notes for World of Warcraft...")
     submissions = wow.submissions(start=submission_interval)
     for submission in submissions:
         if ('Patch Notes - WoW'.lower() in submission.title.lower() 
@@ -81,10 +84,10 @@ def main():
             if submission.id not in foundPosts:
                 submit("World of Warcraft", "PC", submission)
             else:
-                print ("[INFO] Most recent post for World of Warcraft already found...")
+                logging.info("Most recent post for World of Warcraft already found...")
             break
 
-    print ("[INFO] Finished finding patch notes!")
+    logging.info("Finished finding patch notes!")
 
 def submit(game, platform, submission): 
     usedIds = open("posts.txt", "a")
@@ -94,20 +97,18 @@ def submit(game, platform, submission):
             response = reddit.subreddit("patchnotes").submit(formatTitle(game, submission.title, platform), url=submission.url)
             response.reply("Originally posted by " + submission.author.name + "\n\nPlease message me if something is wrong with this post or you have any suggestions!")
             usedIds.write(submission.id + '\n')
-            print ("[INFO] Id '"+submission.id+"' logged.")
+            logging.info("Id '"+submission.id+"' logged.")
         except APIException as e:
             if e.error_type == 'RATELIMIT':
-                sys.stdout.write("[WARNING] %s%%   \r"%(e))
-                sys.stdout.flush()
+                logging.warning(e.message)
             else: 
-                print ("[API ERROR] " + e.message)
+                logging.error("[API ERROR] " + e.message)
                 sys.exit()
             time.sleep(30) 
         except Exception as e: 
-            print ("[UNCAUGHT ERROR] " + e.message)
+            logging.error(e.message)
             sys.exit() 
-    print ("")
-    print ("[INFO] Submission for " + game + " done!")
+    logging.info("Submission for " + game + " done!")
     usedIds.close() 
 
 def formatTitle(game, postTitle, platform): 
